bd226945fc06d77145820b8feefabf6735075dbd
Haixing Hu <starfish.hu@gmail.com>
2015-10-30 19:42:05 +0800

    update demo

---
+++ demo/rename_files.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+declare -r USAGE="\
+Usage: batchrename.sh EXTENSION
+
+  Renames all files under the current directory sequentially.
+
+  For example,
+
+    ./batchrename.sh
+
+  will renames all files in the current directory to \"1.mp3\", \"2.mp3\",
+  ..., \"100.mp3\", ..., etc.
+";
+
+if [ "$1" == "--help" -o "$1" == "-h" ]; then
+  echo "$USAGE";
+  return 1;
+fi
+
+declare file, ext, newfile, i=1;
+for file in *; do
+  ext="${file##*.}";
+  newfile="${i}.${ext}.tmp";
+  echo "rename ${file} to ${newfile}";
+  mv "${file}" "${newfile}";
+  let i=i+1;
+done
+for file in *; do
+  newfile="${file%.*}";
+  echo "rename ${file} to ${newfile}";
+  mv "${file}" "${newfile}";
+done
